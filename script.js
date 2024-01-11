2174c244716f6c475273f40cfa7bce7e2205322c
aishaathmanlali <aishaathmanal@gmail.com>
2024-01-11 00:22:00 +0000

    use map function to display all episodes

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-const seasonNumber = () => {
-  if(getOneEpisode().season < 10){
-    return `S0${getOneEpisode().season}`;
-  }
-  return `S${getOneEpisode().season}`;
-}
+const tvEpisodeDetails = (episode) => {
 
-const episodeNumber = () => {
-  if (getOneEpisode().number < 10) {
-    return `E0${getOneEpisode().number}`;
-  }
-  return `E${getOneEpisode().number}`;
-};
+  const seasonNumber = () => {
+    if (episode.season < 10) {
+      return `S0${episode.season}`;
+    }
+    return `S${episode.season}`;
+  };
 
+  const episodeNumber = () => {
+    if (episode.number < 10) {
+      return `E0${episode.number}`;
+    }
+    return `E${episode.number}`;
+  };
+  
 const seasonAndEpisode = seasonNumber() + episodeNumber();
+
 
 const rootElem = document.getElementById("root");
 
 const card = document.getElementById("tv-episodes").content.cloneNode(true);
 
-card.querySelector("h3").textContent = getOneEpisode().name+" - "+seasonAndEpisode;
-card.querySelector("img").src = getOneEpisode().image.medium;
-card.querySelector("[data-summary]").textContent = getOneEpisode().summary;
-
-
+card.querySelector("h3").textContent = episode.name + " - " + seasonAndEpisode;
+card.querySelector("img").src = episode.image.medium;
+card.querySelector("[data-summary]").textContent = episode.summary;
 rootElem.appendChild(card);
+
+return card;
+}
+
+const episodes = getAllEpisodes();
+
+const allEpisodes = episodes.map(tvEpisodeDetails);
 
 //You can edit ALL of the code here
 // function setup() {
@@ -34,10 +42,8 @@ rootElem.appendChild(card);
 // function makePageForEpisodes(episodeList) {
 //   const rootElem = document.getElementById("root");
 //   // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  
-//   
+
+//
 // }
-
-
 
 // window.onload = setup;
